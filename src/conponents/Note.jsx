0e30dbc509056a286021181848b785517d55f51e
Ashis Bhowmik <ashisbhowmikisp@gmail.com>
2022-08-24 16:54:37 +0530

    important changes

--- src/conponents/Note.jsx
+++ src/conponents/Note.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import "./note.css";
 import db from "../firebase";
 import { useStateValue } from "../StateProvider";
